66b15ac435a6ae3c94680c040cbd8e3f3d678835
Gabriel Rubio <gabriel@Mac-mini-de-Gabriel.local>
2025-05-13 09:29:54 -0500

    prisma-client-js

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,9 @@
-import { PrismaClient } from "@/generated/prisma";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client";
 
 // PrismaClient singleton to prevent multiple instances during hot reloading
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
